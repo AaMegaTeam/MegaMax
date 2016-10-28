50dedda65306c79e0fc182f5f1de2d91eafcd037
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-10-28 07:36:16 -0700

    Update linksup.lua

--- plugins/linksup.lua
+++ plugins/linksup.lua
@@ -7,11 +7,11 @@ function run(msg, matches)
       return ''
        end
 if matches[1]:lower() == "linksp" then
-         local text = "لینک ساپورت ربات تله بیوند:\n"..group_link
+         local text = "لینک ساپورت ربات ماکسیموس پلاس:\n"..group_link
             return reply_msg(msg.id, text, ok_cb, false)
 end
 if matches[1]:lower() == "linksup" then
-         local text = 'ساپورت ربات تله بیوند'
+         local text = 'ساپورت ربات ماکسیموس پلاس'
 local inline_text = "Tap_Here_To_Join_Support"
 return send_api_msg(msg,get_receiver_api(msg),text,true,'md',inline_text,group_link)
 end
